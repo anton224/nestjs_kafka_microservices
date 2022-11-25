78699d0d6646be326dac388a6c401493241cbc08
Antonr <antonr@rapyd.net>
2022-11-25 11:09:56 +0200

    Add auth-microservice

---
+++ apps/payments-microservice/src/app/app.controller.ts
@@ -0,0 +1,17 @@
+import { MakePaymentDto } from '@nestjs-microservices/shared/dto';
+import { Controller, ValidationPipe } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
+
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  getData(){}
+
+  @EventPattern('process_payment')
+  handleProcessPayment(@Payload(ValidationPipe) data: MakePaymentDto) {
+    this.appService.processPayment(data);
+  }
+}
